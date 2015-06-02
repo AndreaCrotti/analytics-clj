dddc955554115be14ec8977b5059993bbfe1a1f5
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-02 14:49:46 +0100

    datomic hello world

--- project.clj
+++ project.clj
@@ -3,5 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-environ "1.0.0"]
+            [lein-expectations "0.0.8"]]
+
+  :profiles {:dev {:dependencies [[expectations "2.0.16"]]}}
+
+  :aliases {"test" ["expectations"]}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter/incanter-charts "1.3.0"]
+                 [com.datomic/datomic-free "0.9.5153"]
                  [gorillalabs/sparkling "1.2.1"]])
--- src/analytics_clj/core.clj
+++ src/analytics_clj/core.clj
@@ -1,6 +1,13 @@
-(ns analytics-clj.core)
+(ns analytics-clj.core
+  (:require [datomic.api :as d]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;TODO: change it to the right backend when necessary
+(def db-uri-base "datomic:mem://")
+
+(defn datom-conn
+  []
+  "Create a connection to the database"
+  (let [uri (str db-uri-base (d/squuid))]
+    (d/delete-database uri)
+    (d/create-database uri)
+    (d/connect uri)))
