72886da2646c096ffad4d4d86d6ac6c18c5a3878
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-02 13:40:48 +0100

    use sparling

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [yieldbot/flambo "0.6.0"]])
+                 [gorillalabs/sparkling "1.2.1"]])
